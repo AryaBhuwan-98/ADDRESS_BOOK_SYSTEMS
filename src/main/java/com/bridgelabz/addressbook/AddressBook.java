80818e3818608490f12d78191f0ef68be4618d6f
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-18 11:46:31 -0800

    Added:Adding program for add new contact in address book system

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,6 +1,20 @@
 package com.bridgelabz.addressbook;
-import java.util.ArrayList;
 
-public class AddressBook {
-        ArrayList<ContactPerson> contactList = new ArrayList<ContactPerson>();
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AddressBook implements AddressBookIF {
+
+	ArrayList<ContactPerson> contactList = new ArrayList<ContactPerson>();
+	
+	public void addContact(ContactPerson person) {
+        contactList.add(person);
     }
+    public void displayContents() {
+
+        Iterator<ContactPerson> iterator = contactList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
