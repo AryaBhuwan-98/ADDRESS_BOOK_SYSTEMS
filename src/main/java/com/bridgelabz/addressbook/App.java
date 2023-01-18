949c6e15fba8eed8115c7f685c1def98e30f1efc
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-18 11:07:06 -0800

    Add:Welcome to the Address book Systems

---
+++ src/main/java/com/bridgelabz/addressbook/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO ADDRESS BOOK SYSTEMS" );
+    }
+}
