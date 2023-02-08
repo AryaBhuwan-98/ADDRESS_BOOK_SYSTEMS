40fc0091a804f075fb12ac24035c86cbc9ea0a26
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-08 13:50:38 -0800

    Add:Adding program to View Person By City or State in Multiple Ddress Book

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -2,7 +2,8 @@ package com.bridgelabz.addressbook;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    public static Scanner scannerObject = new Scanner(System.in);
+   
+	public static Scanner scannerObject = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("---------- Welcome To Address Book Program!! ----------");
 		
